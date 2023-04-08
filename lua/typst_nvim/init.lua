c9eac8ecf406d70a518ed2e16176c9c8480aba79
mabeh19 <mathiasibehrens@gmail.com>
2023-04-08 19:06:39 +0200

    Renamed folder

--- lua/typst_nvim/init.lua
+++ lua/typst_nvim/init.lua
@@ -3,7 +3,7 @@ local session = require "session"
 local M = {}
 
 function M.setup()
-    vim.api.nvim_create_user_command("TypstLaunch", 'lua require("typst").launch()', {})
+    vim.api.nvim_create_user_command("TypstLaunch", 'lua require("typst_nvim").launch()', {})
 end
 
 M.launch = session.launch
