7a9f002709f06a08bece517e0c5b6059e3af7008
mabeh19 <mathiasibehrens@gmail.com>
2023-04-08 20:02:18 +0200

    Forgot to add typst command

--- lua/typst_nvim/init.lua
+++ lua/typst_nvim/init.lua
@@ -5,7 +5,7 @@ function M.setup()
   vim.api.nvim_create_user_command("TypstLaunch", 'lua require("typst_nvim").launch()', {})
 end
 
-M.launch = vim.cmd("terminal -w *.typ && okular *.pdf")
+M.launch = vim.cmd("terminal typst -w *.typ && okular *.pdf")
 
 return M
 
