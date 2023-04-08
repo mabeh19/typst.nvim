d8c1b94de1d817d2c51c7dcdc44311b875cf4c57
mabeh19 <mathiasibehrens@gmail.com>
2023-04-08 19:59:31 +0200

    Refactoring

--- lua/typst_nvim/init.lua
+++ lua/typst_nvim/init.lua
@@ -1,12 +1,11 @@
-local session = require "session"
 
 local M = {}
 
 function M.setup()
-    vim.api.nvim_create_user_command("TypstLaunch", 'lua require("typst_nvim").launch()', {})
+  vim.api.nvim_create_user_command("TypstLaunch", 'lua require("typst_nvim").launch()', {})
 end
 
-M.launch = session.launch
+M.launch = vim.cmd("terminal -w *.typ && okular *.pdf")
 
 return M
 
--- lua/typst_nvim/session.lua
+++ lua/typst_nvim/session.lua
@@ -1,14 +1,7 @@
 
 local M = {}
 
-local function start()
-    vim.cmd("terminal -w *.typ && okular *.pdf")
-end
-
-
 function M.launch()
-    start()
-
 end
 
 return M
