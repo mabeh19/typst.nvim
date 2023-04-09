a6d4cfa619f63146102784b24af7120524d12960
mabeh19 <mathiasibehrens@gmail.com>
2023-04-09 11:57:50 +0200

    Refactoring

---
+++ README.txt
@@ -0,0 +1,5 @@
+
+
+# Typst.nvim
+
+__Very__ small script for automatically watching typst files and viewing them with xdg-open.
--- lua/typst_nvim/init.lua
+++ lua/typst_nvim/init.lua
@@ -2,16 +2,8 @@
 local M = {}
 
 M.launch = function ()
-  --local autocmd = vim.api.nvim_create_autocmd
-  --autocmd("BufWritePost", {
-  --  pattern = "*",
-  --  callback = function()
-  --    vim.cmd("silent! typst *.typ") -- compile saved files
-  --  end
-  --})
-  vim.fn.jobstart("okular *.pdf", {detach = true})
   vim.fn.jobstart("typst -w *.typ", {detach = true})
-  --vim.cmd("!okular *.pdf &")
+  vim.fn.jobstart("xdg-open *.pdf", {detach = true})
 end
 
 return M
