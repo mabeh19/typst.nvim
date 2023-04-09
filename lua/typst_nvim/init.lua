189643705b9f77f6a1b9eb86ae9529868d107bd0
mabeh19 <mathiasibehrens@gmail.com>
2023-04-09 11:51:07 +0200

    Refactoring

--- lua/typst_nvim/init.lua
+++ lua/typst_nvim/init.lua
@@ -2,14 +2,15 @@
 local M = {}
 
 M.launch = function ()
-  local autocmd = vim.api.nvim_create_autocmd
-  autocmd("BufWritePost", {
-    pattern = "*",
-    callback = function()
-      vim.cmd("silent! typst *.typ") -- compile saved files
-    end
-  })
+  --local autocmd = vim.api.nvim_create_autocmd
+  --autocmd("BufWritePost", {
+  --  pattern = "*",
+  --  callback = function()
+  --    vim.cmd("silent! typst *.typ") -- compile saved files
+  --  end
+  --})
   vim.fn.jobstart("okular *.pdf", {detach = true})
+  vim.fn.jobstart("typst *.typ", {detach = true})
   --vim.cmd("!okular *.pdf &")
 end
 
